15a0c5da4515d79cebdad26862292327921f8388
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-07-03 11:31:03 -0600

    added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,23 @@
 <template>
   <div id="app">
     <TaskList/>
-    <TaskItem/>
     <TaskInput/>
   </div>
 </template>
 
 <script>
 import TaskList from './components/TaskList'
-import TaskItem from './components/TaskItem'
 import TaskInput from './components/TaskInput'
 
 export default {
   name: 'app',
   components: {
     TaskList,
-    TaskItem,
     TaskInput
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,9 +1,16 @@
 <template>
-    <h1>Task List</h1>
+    <div>
+        <h1>Task List</h1>
+        <TaskItem/>
+    </div>
 </template>
 
 <script>
+    import TaskItem from './TaskItem'
     export default {
         name: 'TaskList',
+        components: {
+            TaskItem
+        }
     }
 </script>
